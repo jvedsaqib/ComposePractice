aa046f0b04ff10ac8b37db43c79f99967cc5c297
Saqib Javed <jvedsaqib6291@gmail.com>
2023-10-15 19:01:54 +0530

    simpleTextSmall

--- app/src/main/java/com/javed/composepractice/components/TextComponents.kt
+++ app/src/main/java/com/javed/composepractice/components/TextComponents.kt
@@ -1,8 +1,13 @@
 package com.javed.composepractice.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -10,8 +15,19 @@ fun simpleText(msg : String){
     Text(text = msg)
 }
 
+@Composable
+fun simpleTextSmall(msg : String){
+    Text(
+        text = msg,
+        Modifier.size(8.dp)
+    )
+}
+
 @Preview
 @Composable
 fun prev(){
-    simpleText(msg = "Hello")
+    Column(){
+        simpleText(msg = "Hello")
+        simpleTextSmall(msg = "Hi")
+    }
 }
